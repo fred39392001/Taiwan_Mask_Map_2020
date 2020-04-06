089728e3d0880cd38a20af131546b7ef1a28643d
fred39392001 <fred39392001@gmail.com>
2020-04-06 14:51:35 +0800

    Update all.js
    delete the click pharmacy list  popup event utill find a better solution

--- js/all.js
+++ js/all.js
@@ -311,25 +311,6 @@ function clickPharmacyGeo(pharmacyTitle, pharmacyNameList){
             Lat = Number(e.currentTarget.dataset.lat);
             Lng = Number(e.currentTarget.dataset.lng);
             map.setView([Lat, Lng], 20);
-            markerOpen(Lat, Lng);
     })
 }
-}
-
-const markerOpen = (lat, lng) => {
-    // 搜尋 markers 圖層下的子圖層
-    markers.eachLayer(layer => {
-        // 抓取圖層的 經緯度
-        const eachLat = layer._latlng.lat;
-        const eachLng = layer._latlng.lng;
-        // 如果與參數的經緯度相同，就抓取那個 layer
-        if (eachLat === lat && eachLng === lng) {
-            // zoomToShowLayer 這個是 MarkerClusterGroup 給的函式
-            // 方法是調用 MarkerClusterGroup 下的子圖層
-            markers.zoomToShowLayer(layer, () =>
-                // 打開 bindPopup 的 HTML
-                layer.openPopup()
-            );
-        }
-    });
 }
